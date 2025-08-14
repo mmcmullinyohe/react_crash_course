0d4b56722ac99c1d3460f65221221c972b1f44dc
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-08-14 15:28:29 -0500

    remove counter

--- App.jsx
+++ App.jsx
@@ -1,10 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Todo from './components/Todo.jsx'
+import Title from './components/Title.jsx'
+import Modal from './components/Modal.jsx'
+import React, { useState } from 'react';
+import Counter from './components/Counter.jsx';
 
 function App() {
+  const [showModal, setShowModal] = useState(false)
   return (
-    <div className="App">
-
+    <div>
+      <Title />
+      <div>
+        <input type="text" onChange= {(event) => {
+          console.log(event.target.value)
+        }}/>
+        <button onClick={() => setShowModal(true)}>Add Todo</button>
+      </div>
+      <div className="todo__wrapper">
+      <Todo 
+      title="Finish Frontend Simplified" />
+      <Todo 
+      title="Finish Interview Section" />
+      <Todo 
+      title="Land a $100k Job"/>
+      </div>
+      {showModal && <Modal title="Confirm Delete?"/>}
     </div>
   );
 }
---
+++ components/Todo.jsx
@@ -0,0 +1,16 @@
+import './Todo.css'
+
+function Todo({ title }) {
+  function deleteTodo(id) {
+    console.log('deleteTodo()', title.toUpperCase())
+  }
+
+  return (
+    <div className="todo">
+      <p>{title}</p>
+      <button onClick={() => deleteTodo(1)}>Delete</button>
+    </div>
+  );
+}
+
+export default Todo;
