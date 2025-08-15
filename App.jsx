d421d05ba21acbd19bf49a14f49cf30f25f2c8de
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-08-15 14:08:37 -0500

    nav

--- App.jsx
+++ App.jsx
@@ -1,21 +1,20 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home.jsx'
-import About from './pages/About.jsx'
-import Contact from './pages/Contact';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Contact from "./pages/Contact";
+import Nav from "./components/Nav";
 
 function App() {
-
   return (
-    <div>
-     <Router>
+    <Router>
+       <Nav />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
       </Routes>
-     </Router>
-    </div>
+    </Router>
   );
 }
 
