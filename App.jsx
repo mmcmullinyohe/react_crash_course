3358009058c6534311fc99a4aeec6f935d32ffdb
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-08-15 13:06:06 -0500

    pages

--- App.jsx
+++ App.jsx
@@ -2,11 +2,31 @@ import './App.css';
 import Todo from './components/Todo.jsx'
 import Title from './components/Title.jsx'
 import Modal from './components/Modal.jsx'
-import React, { useState } from 'react';
-import Counter from './components/Counter.jsx';
+import React, { useState, useEffect } from 'react';
 
 function App() {
   const [showModal, setShowModal] = useState(false)
+
+  function onTodoDelete() {
+    setShowModal(true)
+  }
+
+  function cancelModal() {
+    setShowModal(false)
+  }
+
+  function confirmModal() {
+    setShowModal(false)
+  }
+
+  useEffect(() => {
+    console.log("ONLY on mount")
+  }, [])
+
+  useEffect(() => {
+    console.log("on mount and on {showModal} change")
+  }, [showModal])
+
   return (
     <div>
       <Title />
@@ -17,14 +37,14 @@ function App() {
         <button onClick={() => setShowModal(true)}>Add Todo</button>
       </div>
       <div className="todo__wrapper">
-      <Todo 
+      <Todo onTodoDelete={onTodoDelete}
       title="Finish Frontend Simplified" />
-      <Todo 
+      <Todo onTodoDelete={onTodoDelete}
       title="Finish Interview Section" />
-      <Todo 
+      <Todo onTodoDelete={onTodoDelete}
       title="Land a $100k Job"/>
       </div>
-      {showModal && <Modal title="Confirm Delete?"/>}
+      {showModal && <Modal cancelModal={cancelModal} confirmModal={confirmModal} title="Confirm Delete?"/>}
     </div>
   );
 }
