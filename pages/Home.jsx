0ec1d014ac1d1f309c4bb69ddcc0775a6052b02a
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-08-18 09:27:49 -0500

    react

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,5 +1,19 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
 function Home() {
-    return <h1>Home</h1>
+    const [users, setUsers] = useState([])
+
+   async function main() {
+           const { data } = await axios.get ("http://jsonplaceholder.typicode.com/users")
+           setUsers(data)
+        }
+
+    useEffect(() => {
+        
+        main()
+    }, [])
+    return <h1>{users[0]?.name}</h1>
 }
 
 export default Home
